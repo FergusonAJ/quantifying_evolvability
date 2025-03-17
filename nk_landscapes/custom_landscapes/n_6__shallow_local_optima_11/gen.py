eef31721c663b92be3d44157f9801c286d11c76a
Austin Ferguson <ferguson.austin.j@gmail.com>
2025-03-17 10:41:50 -0400

    Add more custom landscapes

---
+++ nk_landscapes/custom_landscapes/n_6__shallow_local_optima_11/gen.py
@@ -0,0 +1,18 @@
+L = [0] * (2**6)
+
+# Neutral basin around start
+for i in [0]:
+  L[i] = 10
+
+# Neutral path to global optimum
+for i in [32, 48, 56, 60, 62]:
+  L[i] = 10
+
+# Path to local optimum
+L[1] = 11
+
+# Global optimum
+L[63] = 20
+
+for x in L:
+  print(x)
